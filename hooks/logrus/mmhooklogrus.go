34593a401a5fe5345cc0b07e0662f8154e4fb7a5
Alessandro Sanino <saninoale@gmail.com>
2019-01-04 15:26:54 +0100

    refactoring of level filter + minor bug fixed

--- hooks/logrus/mmhooklogrus.go
+++ hooks/logrus/mmhooklogrus.go
@@ -64,8 +64,8 @@ type mmHookLogrus struct {
 //
 func NewHook(endpoint, channel, username string, attc *Attachment, minLevel logrus.Level) logrus.Hook {
 	levels := make([]logrus.Level, 0, len(logrus.AllLevels))
-	for _, lvl := range logrus.AllLevels {
-		if lvl >= minLevel {
+	for _, lvl := range logrus.AllLevels { // sorted reversally
+		if lvl <= minLevel {
 			levels = append(levels, lvl)
 		}
 	}
@@ -92,6 +92,7 @@ func NewHook(endpoint, channel, username string, attc *Attachment, minLevel logr
 		_hook.channel = channel
 		_hook.username = username
 		_hook.defAttc = attc
+		_hook.levels = levels
 
 		_hookLocker.Unlock()
 	}
@@ -118,10 +119,14 @@ func (hook *mmHookLogrus) Fire(entry *logrus.Entry) (err error) {
 		return
 	}
 
-	msg := NewMessage(hook.Channel(), hook.Username(), hook.Hostname(),
-		hook.Attachment(), entry)
-
-	_chanMsg <- msg
+	for _, lvl := range hook.levels {
+		if lvl == entry.Level {
+			msg := NewMessage(hook.Channel(), hook.Username(), hook.Hostname(),
+				hook.Attachment(), entry)
+			_chanMsg <- msg
+			break
+		}
+	}
 
 	return
 }
